c9e0840b35c82647509218c50e4ea0a2a42eb1c2
Arviixzuh <arviixzuh@gmail.com>
2024-10-10 10:25:25 -0400

    feat(user): ✨ add edit user endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,8 @@
 import { Request } from 'express'
 import { UserService } from './user.service'
+import { EditUserDto } from './dto/edit-user.dto'
 import { ApiBearerAuth } from '@nestjs/swagger'
-import { Controller, Get, Req } from '@nestjs/common'
+import { Body, Controller, Get, Put, Req } from '@nestjs/common'
 
 @Controller('/user')
 @ApiBearerAuth()
@@ -11,5 +12,10 @@ export class UserController {
   @Get('/')
   getHello(@Req() req: Request): Promise<String> {
     return this.appService.getHello(req.user.userId)
+  }
+
+  @Put('/edit')
+  editUser(@Req() req: Request, @Body() body: EditUserDto) {
+    return this.appService.editUser(req.user.userId, body)
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,22 +1,28 @@
-import { User } from '@prisma/client'
+import { EditUserDto } from './dto/edit-user.dto'
 import { PrismaService } from 'src/prisma/prisma.service'
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
+  private async findUserById(id: number) {
+    const user = await this.prisma.user.findUnique({ where: { id } })
+    if (!user) throw new NotFoundException('User not found.')
+
+    return user
+  }
+
   async getHello(id: number): Promise<String> {
-    const user: User = await this.prisma.user.findUnique({
-      where: {
-        id,
-      },
-    })
-
-    if (!user) {
-      throw new HttpException('Usuario no encontrado', HttpStatus.NOT_FOUND)
-    }
-
+    const user = await this.findUserById(id)
     return `Hola ${user.name}`
+  }
+
+  async editUser(id: number, body: EditUserDto) {
+    await this.findUserById(id)
+    return await this.prisma.user.update({
+      where: { id },
+      data: body,
+    })
   }
 }
